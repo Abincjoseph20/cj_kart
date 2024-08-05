61c2d5b07b140b769c6734301228a8d925095230
abin c joseph <abincjoseph77@gmail.com>
2024-08-05 19:42:28 +0530

    Erro_cart2

--- cj_kart/cj_kart/carts/views.py
+++ cj_kart/cj_kart/carts/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render,redirect
 from mainapp.models import Products
 from .models import Carts,Cart_items
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
 
 # Create your views here.
-
-
-
 
 def _cart_id(request): #this private function is using to fetch the session id of a single product
     cart = request.session.session_key
@@ -18,7 +17,7 @@ def add_to_cart(request,product_id): #to get the Product
     product = Products.objects.get(id=product_id)
     try:
         cart = Carts.objects.get(cart_id=_cart_id(request)) #get the cart id using the cart session
-    except Carts.DoesNotExist:
+    except Carts.ObjectDoesNotExist:
         cart = Carts.objects.create(
             cart_id = _cart_id(request)
         )
@@ -29,13 +28,15 @@ def add_to_cart(request,product_id): #to get the Product
         cart_item.quantity += 1 #expantion -> (cart_item.quantity = cart_item.quantity + 1)
         cart_item.save()
 
-    except Cart_items.DoesNotExist: #if there is no Existing product here then add the product to the cart
+    except Cart_items.ObjectDoesNotExist: #if there is no Existing product here then add the product to the cart
         cart_item = Cart_items.objects.create(
             product = product,
             quantity = 1,
             cart = cart,
         )
         cart_item.save()
+    return HttpResponse(cart_item.quantity)
+    exit()
     return redirect('cart')
 
 
@@ -44,14 +45,16 @@ def Cart_view(request,total=0,quantity=0,cart_items=None):  # this function for 
         cart = Carts.objects.get(cart_id=_cart_id(request))
         cart_items = Cart_items.objects.filter(cart=cart,is_active=True)
         for cart_item in cart_items:
-            total += (cart_item.product.price * cart_items.quantity)
+            total += (cart_item.product.discounted_price * cart_item.quantity)
             quantity += cart_item.quantity
-    except ObjectNotExist:
+            # print(cart_items.quantity)
+    except ObjectDoesNotExist:
         pass
     
-    contect = {
+    context = {
         'total':total,
         'quantity':quantity,
         'cart_items':cart_items,
     }
-    return render(request, 'mainapp/cart.html',contect)
+    return render(request, 'mainapp/cart.html', context)
+
